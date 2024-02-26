7d227f9d13a76184ce705ae44a6487ae3e8e3159
serhii.zghama <serhii.zghama@edetek.com>
2024-02-26 16:14:45 +0700

    Implement method get_birthdays_per_week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import defaultdict
-from datetime import datetime
+from collections import defaultdict, OrderedDict
+from datetime import datetime, timedelta
 
 
 def get_birthdays_per_week(users):
@@ -13,23 +13,33 @@ def get_birthdays_per_week(users):
     for user in users:
         birthday = user["birthday"].date()
         birthday_this_year = birthday.replace(year=today.year)
-        
-        if birthday_this_year < today:
-            print()
-        if today <= birthday <= today + timedelta(days=7):
-            day_of_week = birthday.strftime("%A")
-            birthdays_per_week[day_of_week].append(user["name"])
-    
 
-    for day, names in birthdays_per_week.items():
-        print(f"{day}: {', '.join(names)}")
+        if birthday_this_year < today:
+            birthday_this_year = birthday.replace(year=today.year+1)
+
+        if today <= birthday_this_year <= today + timedelta(days=7):
+            day_of_week = birthday_this_year.strftime("%A")
+            if day_of_week == "Saturday" or day_of_week == "Sunday":
+                day_of_week = "Monday"
+            birthdays_per_week[day_of_week].append(user["name"])
+
+    ordered_birthdays_per_week = OrderedDict(
+        (day, birthdays_per_week[day]) for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    )
+
+    for day, names in ordered_birthdays_per_week.items():
+        if names:
+            print(f"{day}: {', '.join(names)}")
 
 
 users = [
     {"name": "Bill Gates", "birthday": datetime(1955, 10, 28)},
-    {"name": "Jill Valentine", "birthday": datetime(1974, 11, 21)},
-    {"name": "Kim Kardashian", "birthday": datetime(1980, 10, 21)},
-    {"name": "Jan Koum", "birthday": datetime(1976, 2, 24)}
+    {"name": "Jill Valentine", "birthday": datetime(1974, 2, 25)},
+    {"name": "Kim Kardashian", "birthday": datetime(1980, 2, 28)},
+    {"name": "Jan Koum", "birthday": datetime(1976, 2, 28)},
+    {"name": "Bob Bobing", "birthday": datetime(1956, 2, 28)},
+    {"name": "Omen Roment", "birthday": datetime(1986, 2, 26)},
+    {"name": "Alex Muller", "birthday": datetime(1976, 2, 26)}
 ]
 
 get_birthdays_per_week(users)
